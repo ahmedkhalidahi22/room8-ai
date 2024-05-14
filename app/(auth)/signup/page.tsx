b1282b063cae9ffc8c08c0d1f538e4f60a9c27bb
ahmedkhalidahi22 <ahmedkhalidahi22@gmail.com>
2024-05-15 00:01:21 +0300

    add zod validator to react-hook-form

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -29,9 +29,6 @@ export default function Home() {
 
   const { register, handleSubmit, formState } = useForm<TformSchema>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: "Ahmed",
-    },
   });
   const onSubmit: SubmitHandler<TformSchema> = (data) => {
     mutation.mutate({ email: data.email, name: data.name, phone: data.phone });
@@ -66,10 +63,7 @@ export default function Home() {
           <div className="grid grid-cols-2 gap-4">
             <div className="space-y-2">
               <label htmlFor="name">Name</label>
-              <input
-                placeholder="John Doe"
-                {...register("name", { required: "HEY, ENTER NAME" })}
-              />
+              <input placeholder="John Doe" {...register("name")} />
 
               <p className=" w-full py-2 px-2  text-red-700">
                 {errors.name?.message}
@@ -77,22 +71,7 @@ export default function Home() {
             </div>
             <div className="space-y-2">
               <label htmlFor="email">Email</label>
-              <input
-                placeholder="example@email.com"
-                {...register("email", {
-                  required: "HEY, ENTER EMAIL",
-                  pattern: {
-                    value: /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/,
-                    message: "Email form is incorrect",
-                  },
-                  validate: (value) => {
-                    return (
-                      !(value == "admin@gmail.com") ||
-                      "This email is not allowed"
-                    );
-                  },
-                })}
-              />
+              <input placeholder="example@email.com" {...register("email")} />
               <p className=" w-full py-2 px-2  text-red-700">
                 {errors.email?.message}
               </p>
@@ -101,17 +80,7 @@ export default function Home() {
           <div className="grid grid-cols-2 gap-4">
             <div className="space-y-2">
               <label htmlFor="phone">Phone Number</label>
-              <input
-                placeholder="+1 (123) 456-7890"
-                {...register("phone", {
-                  required: "HEY, ENTER PHONE",
-                  pattern: {
-                    value:
-                      /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/,
-                    message: "Phone number format is invalid",
-                  },
-                })}
-              />
+              <input placeholder="+1 (123) 456-7890" {...register("phone")} />
               <p className=" w-full py-2 px-2  text-red-700">
                 {errors.phone?.message}
               </p>
