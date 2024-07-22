4d99cbf9f48ae865af8a3145726326a2b5233469
ahmedkhalidahi22 <ahmedkhalidahi22@gmail.com>
2024-07-22 05:55:56 +0300

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,6 @@
 import { auth } from "@/auth";
 
-export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname !== "/login") {
-    const newUrl = new URL("/login", req.nextUrl.origin);
-    return Response.redirect(newUrl);
-  }
-});
+export default auth((req) => {});
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
