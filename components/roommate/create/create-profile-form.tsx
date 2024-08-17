bf51f8e4566abe63e7c35aa3706571d66134b45f
M1zir <abubakra7mad>
2024-08-17 18:32:21 +0400

    create profile form

--- components/roommate/create/create-profile-form.tsx
+++ components/roommate/create/create-profile-form.tsx
@@ -134,7 +134,10 @@ export default function CreateProfileForm() {
   }, [submitResponse]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-12   px-4 sm:px-6 lg:px-8">
+    <div className="flex flex-col items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+      <div
+  className="absolute -z-10 inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"
+></div>
       <div className="w-full max-w-[650px] bg-white rounded-lg shadow-md">
         <div className="px-8 py-6 space-y-6">
           <div className="space-y-3">
