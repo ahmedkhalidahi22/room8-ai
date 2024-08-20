250a77567b72f5ae70143aa6646b64a4c37e7427
ahmedkhalidahi22 <ahmedkhalidahi22@gmail.com>
2024-08-20 06:48:08 +0300

    comment profile code

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -6,19 +6,16 @@ import { parse } from "path";
 import { z } from "zod";
 
 export async function POST(request: Request, response: NextResponse) {
-  const userInfo: unknown = await request.json();
-
-  const session = await auth();
-  const user = session?.user;
-  if (!user) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
-  const parsedUserProfileInfo = FormUserDetailSchema.safeParse(userInfo);
-  if (!parsedUserProfileInfo.success) {
-    return NextResponse.json(parsedUserProfileInfo.error, { status: 422 });
-  }
-
+  // const userInfo: unknown = await request.json();
+  // const session = await auth();
+  // const user = session?.user;
+  // if (!user) {
+  //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  // }
+  // const parsedUserProfileInfo = FormUserDetailSchema.safeParse(userInfo);
+  // if (!parsedUserProfileInfo.success) {
+  //   return NextResponse.json(parsedUserProfileInfo.error, { status: 422 });
+  // }
   // const userProfile = await prisma.profile.create({
   //   data: {
   //     age: parseInt(parsedUserProfileInfo.data.age),
@@ -28,9 +25,7 @@ export async function POST(request: Request, response: NextResponse) {
   //     userId: user.id as string,
   //   },
   // });
-
   // console.log("user Data after posting: ", userProfile);
-
   // return NextResponse.json({
   //   message: "user details created successfully",
   //   userDetail: userProfile.id,
