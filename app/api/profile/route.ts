986a79aa6c28ff0b99d63d82bd30ee1228ec486f
ahmedkhalidahi22 <ahmedkhalidahi22@gmail.com>
2024-08-20 05:43:03 +0300

    comment userProfile create code

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
-import { userDetailSchema } from "@/lib/validations";
+import { FormUserDetailSchema } from "@/lib/validations";
 import { NextResponse } from "next/server";
 import { parse } from "path";
 import { z } from "zod";
@@ -14,25 +14,25 @@ export async function POST(request: Request, response: NextResponse) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  const parsedUserProfileInfo = userDetailSchema.safeParse(userInfo);
+  const parsedUserProfileInfo = FormUserDetailSchema.safeParse(userInfo);
   if (!parsedUserProfileInfo.success) {
     return NextResponse.json(parsedUserProfileInfo.error, { status: 422 });
   }
 
-  const userProfile = await prisma.profile.create({
-    data: {
-      age: parseInt(parsedUserProfileInfo.data.age),
-      gender: parsedUserProfileInfo.data.gender == "male" ? true : false,
-      nationality: parsedUserProfileInfo.data.nationality,
-      occupation: parsedUserProfileInfo.data.occupation,
-      userId: user.id as string,
-    },
-  });
+  // const userProfile = await prisma.profile.create({
+  //   data: {
+  //     age: parseInt(parsedUserProfileInfo.data.age),
+  //     gender: parsedUserProfileInfo.data.gender == "male" ? true : false,
+  //     nationality: parsedUserProfileInfo.data.nationality,
+  //     occupation: parsedUserProfileInfo.data.occupation,
+  //     userId: user.id as string,
+  //   },
+  // });
 
-  console.log("user Data after posting: ", userProfile);
+  // console.log("user Data after posting: ", userProfile);
 
-  return NextResponse.json({
-    message: "user details created successfully",
-    userDetail: userProfile.id,
-  });
+  // return NextResponse.json({
+  //   message: "user details created successfully",
+  //   userDetail: userProfile.id,
+  // });
 }
--- components/landing/landing.tsx
+++ components/landing/landing.tsx
@@ -1,27 +1,24 @@
-import { Button } from "@/components/ui/button"
-import Link from 'next/link'
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import Particles from "@/components/ui/particles";
 export default function Landing() {
   return (
-<div className="flex flex-col items-center justify-center h-screen">
-<div
-  className="absolute -z-10 inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"
-></div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="absolute -z-10 inset-0 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
       <div className="space-y-2 mb-5 text-center">
         <h1 className="text-6xl font-bold ">Find a Roommate</h1>
-        <h2 className="text-xl text-muted-foreground"> just tell us what you need and we will help you using AI</h2>
+        <h2 className="text-xl text-muted-foreground">
+          {" "}
+          just tell us what you need and we will help you using AI
+        </h2>
       </div>
       <div className="mt-3 w-full text-center">
         <div className="flex flex-row gap-4 justify-center">
           <Button variant="accent" size="lg">
-            <Link href={"/roommate/create"}>
-              Searching for a place
-            </Link>
+            <Link href={"/roommate/create"}>Searching for a place</Link>
           </Button>
           <Button variant="outline" size="lg">
-            <Link href={"/"}>
-              Need a roommate
-            </Link>
+            <Link href={"/"}>Need a roommate</Link>
           </Button>
         </div>
       </div>
@@ -31,6 +28,7 @@ export default function Landing() {
         ease={80}
         color={"#065f46"}
         refresh
-      />    </div>
-  )
+      />{" "}
+    </div>
+  );
 }
